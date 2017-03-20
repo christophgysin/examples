01f80ac09aea69ee19c54e104286873526e5b538
Christoph Gysin <christoph.gysin@gmail.com>
2017-03-20 09:41:28 +0200

    fix readme check

--- check-if-readme-is-up-to-date.sh
+++ check-if-readme-is-up-to-date.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Checking if README.md has been updated..."
-
-if ! git diff-index --quiet HEAD README.md; then
+echo
+git diff
+echo
+diff=$(git diff-index HEAD -- README.md)
+if [[ -n $diff ]]; then
     echo "README.md needs to be regenerated!"
     echo
     echo "Please run:"
     echo "  $ npm run docs"
     exit 1
 fi
+
+exit 0
